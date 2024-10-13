19e4797bacc921eaba0388981a8652c456aa7ee5
cot.nyashniy@yandex.ru <cot.nyashniy@yandex.ru>
2024-10-14 03:40:22 +0400

    Поддержка нескольких стратегий с выбором. Возможность обновить Zapret-bundle. Контроль версии (не автообновление, а просто неназойливый контроль версии) Мелкие изменения

--- LazyDisYTUnloker/Properties/Settings.Designer.cs
+++ LazyDisYTUnloker/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LazyDisYTUnloker.Properties {
+namespace LazyDisYTUnlocker.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
